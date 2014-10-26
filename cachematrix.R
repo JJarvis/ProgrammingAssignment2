217e328fc0696dc99ef732f8b61dad94043e702c
JJarvis <james_jarvis@hotmail.com>
2014-10-26 23:03:52 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix - set up the list that contains the accessors for the matrix in question
+## cacheSolve - return the cached inverse if it hs already been instantiated, otherwise compue inv chache it and return it
 
-## Write a short comment describing this function
+## makeCacheMatrix - set up the list that contains the accessors for the matrix in question
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - return the cached inverse if it hs already been instantiated, otherwise compue inv chache it and return it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
